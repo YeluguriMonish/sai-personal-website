329965affb698563fe79dc23761ae485861539f5
Quelpo <yeluguri.monish@gmail.com>
2021-06-02 23:15:58 -0400

    basic routing implemented

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
-import Header from "./navbar/Header.js";
+import Header from "./header/Header.js";
+import Blog from "./blog/Blog.js";
+import Home from "./home/Home.js";
+import Resume from "./resume/Resume.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class Parent extends Component {
   render() {
-    return <Header />;
+    return (
+      <React.Fragment>
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/resume" component={Resume} />
+          </Switch>
+        </Router>
+      </React.Fragment>
+    );
   }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,12 +8,12 @@ export default class Header extends Component {
   render() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">SC</Navbar.Brand>
+        <Navbar.Brand href="/">SC</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#blog">Blog</Nav.Link>
-            <Nav.Link href="#resume">Resume</Nav.Link>
+            <Nav.Link href="blog">Blog</Nav.Link>
+            <Nav.Link href="resume">Resume</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
