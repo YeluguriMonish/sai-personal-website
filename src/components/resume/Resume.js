790be30142e84679bf23fe7e517b9b9241e17f88
Quelpo <yeluguri.monish@gmail.com>
2021-06-03 23:32:10 -0400

    implemented resume template

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,49 +1,15 @@
 import React, { Component } from "react";
-import { Grid, Menu, Segment } from "semantic-ui-react";
-
+import Resume from "react-awesome-resume";
+import "semantic-ui-css/semantic.min.css";
 import "./Resume.css";
+import ResumeInfo from "./ResumeInfo.js";
 
-export default class Resume extends Component {
-  state = { activeItem: "bio" };
-
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+export default class Resumex extends Component {
   render() {
-    const { activeItem } = this.state;
-
     return (
-      <Grid>
-        <Grid.Column width={4} className="tab">
-          <Menu fluid vertical tabular>
-            <Menu.Item
-              name="bio"
-              active={activeItem === "bio"}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name="pics"
-              active={activeItem === "pics"}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name="companies"
-              active={activeItem === "companies"}
-              onClick={this.handleItemClick}
-            />
-            <Menu.Item
-              name="links"
-              active={activeItem === "links"}
-              onClick={this.handleItemClick}
-            />
-          </Menu>
-        </Grid.Column>
-
-        <Grid.Column stretched width={12}>
-          <Segment>
-            This is an stretched grid column. This segment will always match the
-            tab height
-          </Segment>
-        </Grid.Column>
-      </Grid>
+      <div className="template">
+        <Resume jsonResume={ResumeInfo} theme="default" inline={true} />
+      </div>
     );
   }
 }
